045668661c11e91e75fde3db7ed6c2d1bd09be9f
maumau1998 <kemillyleite85@gmail.com>
2022-08-31 13:45:25 -0300

    exercicio-001

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+const nome = prompt("Digite seu nome !!!")
+const comida1 = prompt("Digite a sua comida favorita !!!")
+const comida2 = prompt("Digite outra comida favorita !!!")
+const comida3 = prompt("Digite a terceira comidad favorita !!!")
+
+console.log(`Estas são as comidas favoritas de ${nome} :\n -${comida1}\n -${comida2}\n -${comida3}`)
