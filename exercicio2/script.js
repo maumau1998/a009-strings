3c2b950c95f2238519df8e708fe5eae01a8ba98e
maumau1998 <kemillyleite85@gmail.com>
2022-08-31 13:59:12 -0300

    exercicio-002

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,9 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+console.log("Antes da remoção dos espaços :",minhaString.length)
+
+console.log("Depois da remoção dos espaços :",minhaString.trim())
+
+console.log(minhaString.length)
+
+console.log (minhaString.replace("________", "sticioso"))
